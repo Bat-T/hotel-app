ecc71258e138047af95c7fb69dee916d0b294bc8
Bat-T <tamal.malda2010@gmail.com>
2024-05-29 01:59:36 +0530

    added validation

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
+  public reservations: Reservation[] = [];
   constructor() { }
+
+  getReservations() : Reservation[] {
+    return this.reservations; 
+  }
+
+  getRservation(id: string) : Reservation | undefined {
+    return this.reservations.find(r => r.id === id);
+  }
+
+  addReservation(reservation: Reservation) : void {
+    this.reservations.push(reservation);
+  }
+
+  updateReservation(reservation: Reservation) : void {
+    const index = this.reservations.findIndex(r => r.id === reservation.id);
+    this.reservations[index] = reservation;
+  }
+
+  deleteReservation(id: string):void{
+    const index = this.reservations.findIndex(r => r.id === id);
+    this.reservations.splice(index, 1);
+  }
+  
 }
